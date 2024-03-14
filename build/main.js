d83c2817e6756ef6fff3d2ff5883eeae3c20107d
Mikhail Dziarkach <m.dziarkach@godeltech.com>
2024-03-14 09:58:45 +0100

    Remove build folder

--- build/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_js_1 = require("./app.js");
-const bootstrap = () => {
-    const app = new app_js_1.App(3000);
-    app.init();
-};
-bootstrap();
-//# sourceMappingURL=main.js.map
